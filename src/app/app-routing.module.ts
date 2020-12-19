d976bdc1c29c5e55b1c485f2059be25da0723ea7
stein666 <codigodota2@gmail.com>
2020-12-19 02:33:53 -0500

    Cascaron de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+import { TemplateComponent } from './pages/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'reactivo', component: ReactiveComponent},
+  {path:'template', component: TemplateComponent},
+  {path:'**', pathMatch:'full', redirectTo: 'reactivo'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
